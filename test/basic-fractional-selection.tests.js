0edf60b7dd2b76b6b5b68ca6f2bfa3b323dc2283
Jan Miksovsky <jan@quickui.org>
2015-03-09 10:57:58 -0700

    More unit tests

--- test/basic-fractional-selection.tests.js
+++ test/basic-fractional-selection.tests.js
@@ -2,16 +2,66 @@ suite('basic-fractional-selection', function() {
 
   this.timeout(2000);
 
-  var container = document.getElementById('container');
-
-  teardown(function() {
-    container.innerHTML = '';
-  });
-
-  test('selectedIndex is initially zero', function(done) {
+  test('selectedIndex is initially zero', function() {
     var fixture = document.createElement('basic-fractional-selection');
     assert.equal(fixture.selectedIndex, 0);
-    done();
+  });
+
+  test('next increments selectedIndex', function() {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.next();
+    assert.equal(fixture.selectedIndex, 1);
+  });
+
+  test('next respects maximum', function() {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.selectedIndex = 9;
+    fixture.maximum = 9;
+    fixture.next();
+    assert.equal(fixture.selectedIndex, 9);
+  });
+
+  test('previous decrements selectedIndex', function() {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.selectedIndex = 1;
+    fixture.previous();
+    assert.equal(fixture.selectedIndex, 0);
+  });
+
+  test('previous respects minimum', function() {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.previous();
+    assert.equal(fixture.selectedIndex, 0);
+  });
+
+  test('setting maximum below selectedIndex updates selectedIndex', function(done) {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.selectedIndex = 5;
+    fixture.maximum = 3;
+    flush(function() {
+      assert.equal(fixture.selectedIndex, 3);
+      done();
+    });
+  });
+
+  test('setting minimum above selectedIndex updates selectedIndex', function(done) {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.minimum = 1;
+    flush(function() {
+      assert.equal(fixture.selectedIndex, 1);
+      done();
+    });
+  });
+
+  test('snap rounds to nearest index', function(done) {
+    var fixture = document.createElement('basic-fractional-selection');
+    fixture.selectedIndex = 0.5;
+    flush(function() {
+      fixture.snap();
+      assert.equal(fixture.selectedIndex, 1);
+      done();
+    });
+
   });
 
 });
