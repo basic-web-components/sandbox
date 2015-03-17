8bae0fb82372fff2011228cef7c9776d3ae80299
Jan Miksovsky <jan@quickui.org>
2015-03-17 15:46:28 -0700

    BasicAccessibilityHelpers -> BasicAccessibility. Quick basic-cursor-key-navigation demo.

--- BasicAccessibility.js
+++ BasicAccessibility.js
@@ -1,4 +1,4 @@
-var BasicAccessibilityHelpers = {
+var BasicAccessibility = {
 
   addFocusIndicatorStyles: function(element) {
     var target = element || this;
@@ -9,6 +9,14 @@ var BasicAccessibilityHelpers = {
     target.addEventListener('blur', function() {
       this.style.outline = '';
     }.bind(this));
+  },
+
+  makeFocusable: function(element) {
+    var target = element || this;
+    if (target.getAttribute('tabIndex') == null) {
+      // Apply default tabIndex of zero, which puts element in document order.
+      target.setAttribute('tabIndex', 0);
+    }
   },
 
   _focusOutlineStyle: '1px dotted', /* Safari/IE */
